ef18711777d0090f6d635dcd373672a267246488
sirhbira <souhail.irb28@gmail.com>
2023-05-15 10:35:41 +0200

    ra

--- src/main/java/com/example/sae/controleur/Controleur.java
+++ src/main/java/com/example/sae/controleur/Controleur.java
@@ -1,14 +1,13 @@
 package com.example.sae.controleur;
 
-import com.example.sae.Main;
+
+import com.example.sae.vue.TerrainVue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import com.example.sae.modele.Ennemi;
 import com.example.sae.modele.Terrain;
 
@@ -33,14 +32,14 @@ public class Controleur implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        int tileSize = 70;
+        int tileSize = 150;
         Terrain t = new Terrain(tileSize); // initialisation du terrain avec une taille de case de 32
+        TerrainVue tv = new TerrainVue(t, tilePane);
+        tv.afficherTerrain();
 
         personnage = new Ennemi(0, 0, 40);
 
 
-
-        afficherTerrain(t.getTileMap(), tileSize);
 
     }
 
@@ -72,31 +71,5 @@ public class Controleur implements Initializable {
     }
 
 
-    private void afficherTerrain(int[][] terrain, int tileSize) {
-        URL urlImageSol = Main.class.getResource("sol.png");
-        Image imageSol = new Image(String.valueOf(urlImageSol));
 
-        URL urlImageEnn = Main.class.getResource("Alien.png");
-        Image imageEnn = new Image(String.valueOf(urlImageEnn));
-
-        URL urlImageSol2 = Main.class.getResource("sol2.png");
-        Image imageSol2 = new Image(String.valueOf(urlImageSol2));
-
-        URL urlImageStation = Main.class.getResource("station.png");
-        Image imageStation = new Image(String.valueOf(urlImageStation));
-        for (int y = 0; y < terrain.length; y++) {
-            for (int x = 0; x < terrain[y].length; x++) {
-                if (terrain[y][x] == 0) {
-                    ImageView iv3 = new ImageView(imageSol2);
-                    this.tilePane.getChildren().add(iv3);
-                } else if (terrain[y][x] == 1) {
-                    ImageView iv1 = new ImageView(imageSol);
-                    this.tilePane.getChildren().add(iv1);
-                } else if (terrain[y][x] == 2) {
-                    ImageView iv4 = new ImageView(imageStation);
-                    this.tilePane.getChildren().add(iv4);
-                }
-            }
-        }
-    }
 }
--- src/main/java/com/example/sae/vue/TerrainVue.java
+++ src/main/java/com/example/sae/vue/TerrainVue.java
@@ -1,24 +1,51 @@
 package com.example.sae.vue;
 
+import com.example.sae.Main;
 import com.example.sae.modele.Terrain;
 import com.example.sae.modele.Ennemi;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+import java.net.URL;
 
 
 public class TerrainVue {
+    private TilePane tilePane;
     private Terrain terrain;
     private Ennemi ennemi;
 
-    public TerrainVue(Terrain terrain) {
+    public TerrainVue(Terrain terrain, TilePane tilePane) {
         this.terrain = terrain;
+        this.tilePane = tilePane;
         this.ennemi = new Ennemi(0, 0, 1.0); // Initialisation de l'ennemi à la position (0, 0) et vitesse 1.0
     }
 
-    public void afficherTerrain(){
-        // Affichage du terrain
-        int[][] tileMap = terrain.getTileMap();
-        for (int i = 0; i < tileMap.length; i++) {
-            for (int j = 0; j < tileMap[0].length; j++) {
-                // Affichage de la tuile
+    public void afficherTerrain() {
+        int[][] codesTuiles = this.terrain.getTileMap();
+        URL urlImageSol = Main.class.getResource("sol.png");
+        Image imageSol = new Image(String.valueOf(urlImageSol));
+
+        URL urlImageEnn = Main.class.getResource("Alien.png");
+        Image imageEnn = new Image(String.valueOf(urlImageEnn));
+
+        URL urlImageSol2 = Main.class.getResource("sol2.png");
+        Image imageSol2 = new Image(String.valueOf(urlImageSol2));
+
+        URL urlImageStation = Main.class.getResource("station.png");
+        Image imageStation = new Image(String.valueOf(urlImageStation));
+        for (int y = 0; y < codesTuiles.length; y++) {
+            for (int x = 0; x < codesTuiles[y].length; x++) {
+                if (codesTuiles[y][x] == 0) {
+                    ImageView iv3 = new ImageView(imageSol2);
+                    this.tilePane.getChildren().add(iv3);
+                } else if (codesTuiles[y][x] == 1) {
+                    ImageView iv1 = new ImageView(imageSol);
+                    this.tilePane.getChildren().add(iv1);
+                } else if (codesTuiles[y][x] == 2) {
+                    ImageView iv4 = new ImageView(imageStation);
+                    this.tilePane.getChildren().add(iv4);
+                }
             }
         }
     }
